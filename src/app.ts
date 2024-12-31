bb9d1d5579d76e0720a2f95bee050b7ed84f2764
asuselessbrain <arfan18@cse.pstu.ac.bd>
2024-12-31 19:51:26 +0600

    student router created

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,14 @@
 import express, { Request, Response } from 'express';
+import { studentRouter } from './modules/student/student.router';
+import cors from 'cors';
 const app = express();
+
+app.use(express.json());
+app.use(cors())
+
+
+app.use('/api/students', studentRouter);
+
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({
